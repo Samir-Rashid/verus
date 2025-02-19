c5d614d8449b7f9773c86dedaa095fbeff65897e
Samir Rashid <Samir-Rashid@godsped.com>
2025-02-19 18:41:57 +0000

    add eric fixes

---
+++ shell-setup.sh
@@ -0,0 +1,15 @@
+nix-shell -p rustup z3_4_12
+fish
+cd source/
+source ../tools/activate.fish
+set VERUS_Z3_PATH $(which z3)
+ln -sf $(which z3) ./z3
+
+#### https://github.com/verus-lang/verus/blob/main/BUILD.md
+# Build verus trunk from source
+vargo build --release
+
+## Verus binary global path
+# /home/mod/Documents/github/verus-tock/verus/source/target-verus/release/verus
+
+set PATH "/home/mod/Documents/github/verus-tock/verus/source/target-verus/release/:$PATH"
