a615a5cbf8b2dec0d5a40d6cb57f6be75207a7db
Samir Rashid <Samir-Rashid@godsped.com>
2025-02-19 18:42:10 +0000

    fix dep setup

--- shell-setup.sh
+++ shell-setup.sh
@@ -1,5 +1,5 @@
-nix-shell -p rustup z3_4_12
-fish
+# nix-shell -p rustup z3_4_12
+# fish
 cd source/
 source ../tools/activate.fish
 set VERUS_Z3_PATH $(which z3)
@@ -7,9 +7,16 @@ ln -sf $(which z3) ./z3
 
 #### https://github.com/verus-lang/verus/blob/main/BUILD.md
 # Build verus trunk from source
-vargo build --release
+vargo build --release --no-solver-version-check
 
 ## Verus binary global path
 # /home/mod/Documents/github/verus-tock/verus/source/target-verus/release/verus
 
 set PATH "/home/mod/Documents/github/verus-tock/verus/source/target-verus/release/:$PATH"
+
+cd ..
+
+# open verus-tock
+cd ..
+code .
+
