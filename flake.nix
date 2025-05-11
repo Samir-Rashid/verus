ac8cf567c8f1a1ff1320f86898cef9629cc799da
Samir Rashid <Samir-Rashid@godsped.com>
2025-05-10 22:02:02 -0700

    update IDE opening for cargo verus

--- flake.nix
+++ flake.nix
@@ -5,31 +5,35 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-
   outputs = { self, nixpkgs }: let
-	  pkgs = import nixpkgs {
+    pkgs = import nixpkgs {
             system = "x86_64-linux";
-	  };
+    };
    in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       # Use the same mkShell as documented above
-        packages = with pkgs; [
-          rustup
-          z3_4_12
+      packages = with pkgs; [
+        rustup
+        z3_4_12 # Thanks, Morgan :)
+
+        # For building verus-analyzer extension
+        # pkg-config openssl openssl.dev
       ];
       shellHook = ''
-	      # fish
-	      # cd source/
-	      # source ../tools/activate.fish
-	      # set VERUS_Z3_PATH $(which z3)
-	      # ln -sf $(which z3) ./z3
+        # fish
+        # cd source/
+        # source ../tools/activate.fish
+        # set VERUS_Z3_PATH $(which z3)
+        # ln -sf $(which z3) ./z3
 
-              # # Build verus trunk from source
-	      # vargo build --release
+        # # Build verus trunk from source
+        # vargo build --release
 
-              # ## Verus binary global path
-	      # set PATH "/home/mod/Documents/github/verus-tock/verus/source/target-verus/release/:$PATH"
+        # ## Verus binary global path
+        # set PATH "/home/mod/Documents/github/verus-tock/verus/source/target-verus/release/:$PATH"
       '';
+      # For building verus-analyzer extension
+      # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
     };
   };
 }
